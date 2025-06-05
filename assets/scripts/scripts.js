d9ea98fd2b3292d1716fbafd36aaf4a1080f652b
alexandre-mateos <mateosalexandre95@gmail.com>
2025-06-05 14:07:08 +0200

    ajout de la fonction qui affiche un seul cocktail aleatoire à la fois

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -1,8 +1,31 @@
+let form = document.querySelector("#formulaire");
 let searchCocktails = document.querySelector("#recherche-cocktails");
 let myCocktails = document.querySelector("#affichage-cocktails");
+let randomButton = document.querySelector("#aleatoire");
+
+form.addEventListener("submit", (e) => {
+    
+
+    e.preventDefault();
+
+    myCocktails.innerHTML = "";
+
+    let userRequest = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=" + searchCocktails.value;
+    fetchCocktails(userRequest);
+})
+
+randomButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    myCocktails.innerHTML = "";
+    let userRandomRequest = `https://www.thecocktaildb.com/api/json/v1/1/random.php`
+    
+    fetchRandomCocktails (userRandomRequest);
+})
+
 
 function fetchCocktails(query){
-    fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?s="+query).then((response) => {
+    fetch(query).then((response) => {
+        
         return response.json();
     }).then((data) => {
         console.log(data);
@@ -10,14 +33,24 @@ function fetchCocktails(query){
     })
 }
 
+function fetchRandomCocktails(query){
+    fetch(query).then((response) => {
+        return response.json();
+    }).then((data) => {
+        console.log(data);
+        displayCocktails(data.drinks);
+    })
+}
 
 function displayCocktails(drinks){
     for (let i = 0 ; i < drinks.length ; i++){
-        console.log(drinks[i].strDrink)
+        
+        let column = document.createElement("div");
+        column.classList.add("col-3");
         
         // creation de la carte
         let card = document.createElement("div");
-        card.classList.add("card", "card-container");
+        card.classList.add("card", "card-container", "container-carte");
         
 
         // on récupère les image des cocktails à afficher dans la carte et on les attaches à card
@@ -35,10 +68,20 @@ function displayCocktails(drinks){
         paraTitleCocktail.classList.add("card-title");
         paraTitleCocktail.innerHTML = drinks[i].strDrink;
         cardBody.insertAdjacentElement("beforeend", paraTitleCocktail);
-
-        // on récupère tous les ingrédients?
         
-
+        // On récupère les ingrédients, on crée un paragraphe par ingrédient et on applique le style card-text
+        for(j = 1; j < 16; j++){
+            let standardObj = drinks[i];
+            let stringIngredientLoop = 'strIngredient' + j;
+            if(standardObj[stringIngredientLoop]){
+                
+                let newIngredient = document.createElement("p");
+                newIngredient.classList.add("card-text");
+                newIngredient.innerHTML = standardObj[stringIngredientLoop];
+                cardBody.insertAdjacentElement("beforeend", newIngredient);
+            }
+        }
+        
         // on crée l'élément qui accueil les instructions et on l'attache à cardBody
         let paraInstruCocktail = document.createElement("p");
         paraInstruCocktail.classList.add("card-text");
@@ -50,10 +93,26 @@ function displayCocktails(drinks){
         // je rattache cardBody a card
         card.insertAdjacentElement("beforeend", cardBody);
 
+        // j'attache ma card dans ma div column
+        column.insertAdjacentElement("beforeend", card);
+
         // j'attache card à ma page HTML via myCocktail ("#affichage-cocktails")
-        myCocktails.insertAdjacentElement("beforeend", card);
+        myCocktails.insertAdjacentElement("beforeend", column);
+
+        // autre écriture possible pour générer les éléments html. Plus lisible pour de gros éléments :
+        // myCocktails.insertAdjacentHTML('beforeend', `
+        //     <div class="card">
+        //     ${drinks[i].strInstructionsFR}
+        //     </div>`)
+
     }
     
-}
 
-console.log(fetchCocktails("vodka"));
+
+
+
+
+
+
+
+}
